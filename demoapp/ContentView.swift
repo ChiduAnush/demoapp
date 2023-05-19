5b0f0f3671fe3dd507abb99f46498fdc0681b6ff
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 11:47:39 +0530

    added image and resized

--- demoapp/ContentView.swift
+++ demoapp/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image("blackcat")
+                .resizable()
+                .cornerRadius(10)
+                .aspectRatio(contentMode: .fit)
+                .padding(.all)
+
         }
-        .padding()
+
     }
 }
 
