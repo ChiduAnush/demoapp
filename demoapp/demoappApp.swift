b635579353d9e743d330b8375b3674034830244f
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 10:21:32 +0530

    Initial Commit

---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by Chidu Anush on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
